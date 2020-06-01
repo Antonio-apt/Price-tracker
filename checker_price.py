f97ddae552b40a959d4a83f847424a9d2ff18c1c
Antonio Souza <antonios16020@gmail.com>
2020-06-01 11:26:56 -0300

    update float_price function

--- checker_price.py
+++ checker_price.py
@@ -16,10 +16,6 @@ headers = {
     "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'
 }
 
-def float_price(price):
-    price = price[2:].replace('.','').replace(',','.')
-    return float(price)
-
 def send_mail(product, new_price):
     print(os.getenv("EMAIL"))
     server = smtplib.SMTP('64.233.184.108')
@@ -62,7 +58,7 @@ def checker():
                 price = soup.find('span', {"class": re.compile("price*")}).get_text()
                 if not price:
                     price = soup.find('span', {"class": re.compile("sales-price")}).get_text()
-                price = float_price(price)
+                price = sd.float_price(price)
 
                 if(price<i[3]):
                     send_mail(i,price)
